0439df61efd9a603cc1c398d54fa253a3f5f036c
Muhammad-Kashif-Soft-Eng <muhammad.kashif.soft.eng@gmail.com>
2026-06-25 14:31:14 +0500

    Security measures is configured and changes.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, forgotPassword, resetPassword } = require("../controllers/user.controller");
+const { registerUser, loginUser, forgotPassword, resetPassword, logoutUser } = require("../controllers/user.controller");
 
 router.post("/register", registerUser );
 router.post("/login", loginUser );
+router.post("/logout", logoutUser );
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
 
