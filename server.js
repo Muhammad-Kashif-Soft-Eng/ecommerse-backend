a844a1e4be2ce4b066f86372d313ae1575f2f585
Muhammad-Kashif-Soft-Eng <muhammad.kashif.soft.eng@gmail.com>
2026-06-25 13:31:24 +0500

    Security measures is configured and changes.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
 app.use(cors({
     origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    // credentials: true
+    credentials: true
 }));
 
 app.use(cookieParser());
